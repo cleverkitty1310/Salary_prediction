e608b9f2dbfbac510177f99d9ba4bd3c7d25ce93
Benjamin Perry <benjamin950423@gmail.com>
2022-09-25 20:43:30 -0400

    development created

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LassoCV
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import ElasticNetCV
+from sklearn.metrics import mean_squared_error
+
+sns.set(rc={'figure.figsize': [15, 7]}, font_scale=1.2)
+sns.set_style('whitegrid')
+
+import warnings
+warnings.filterwarnings('ignore')
